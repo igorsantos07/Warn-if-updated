76f209305b8d8e9c7afbd5ecc5283dab74630236
Igor Santos <isantos@i-hunter.net>
2012-07-06 19:23:04 -0300

    * paths again

--- protected/config/_local_db.php
+++
@@ -1,6 +0,0 @@
-<? return array(
-	'connectionString' => "sqlite:protected/data/development.db",
-	'emulatePrepare' => true,
-	'charset' => 'utf8',
-	'schemaCachingDuration' => 60*60*24*365, //eternal caching. if there's a need to clean this cache, use a method for it
-);
