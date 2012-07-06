6808eb028ae329b25312fda12f2b89595b4c4d5b
igoru <igorsantos07@gmail.com>
2012-07-06 19:30:54 -0300

    * cleaning footer

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -47,9 +47,8 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+		Copyright &copy; <?php echo date('Y'); ?> by <a href="http://www.igorsantos.com.br">Igor Santos</a><br/>
+		All Rights Reserved<br/>
 	</div><!-- footer -->
 
 </div><!-- page -->
