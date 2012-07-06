308137c09cec1fa945b99028eccfb17b15a9ba7a
Igor Santos <isantos@i-hunter.net>
2012-07-06 18:32:59 -0300

    * more cleaning

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -12,19 +12,12 @@ class SiteController extends Controller {
 		);
 	}
 
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
 	public function actionIndex() {
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		$this->render('index');
 	}
 
-	/**
-	 * This is the action to handle external exceptions.
-	 */
 	public function actionError() {
 		if ($error = Yii::app()->errorHandler->error) {
 			if (Yii::app()->request->isAjaxRequest)
@@ -32,29 +25,6 @@ class SiteController extends Controller {
 			else
 				$this->render('error', $error);
 		}
-	}
-
-	/**
-	 * Displays the contact page
-	 */
-	public function actionContact() {
-		$model = new ContactForm;
-		if (isset($_POST['ContactForm'])) {
-			$model->attributes = $_POST['ContactForm'];
-			if ($model->validate()) {
-				$name = '=?UTF-8?B?' . base64_encode($model->name) . '?=';
-				$subject = '=?UTF-8?B?' . base64_encode($model->subject) . '?=';
-				$headers = "From: $name <{$model->email}>\r\n" .
-						"Reply-To: {$model->email}\r\n" .
-						"MIME-Version: 1.0\r\n" .
-						"Content-type: text/plain; charset=UTF-8";
-
-				mail(Yii::app()->params['adminEmail'], $subject, $model->body, $headers);
-				Yii::app()->user->setFlash('contact', 'Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact', array('model' => $model));
 	}
 
 	/**
