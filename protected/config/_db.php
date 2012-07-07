84aff2b121d7ba2ed439d50acc9cc51b701a1cf5
Igoru <igorsantos07@gmail.com>
2012-07-07 02:15:13 -0300

    + fixing sqlite path problem

--- protected/config/_db.php
+++ protected/config/_db.php
@@ -13,7 +13,7 @@ if (ENV == 'pagoda') {
 }
 else {
 	return array(
-		'connectionString' => "sqlite:data/development.db",
+		'connectionString' => "sqlite:protected/data/development.db",
 		'emulatePrepare' => true,
 		'charset' => 'utf8',
 		'schemaCachingDuration' => 60*60*24*365, //eternal caching. if there's a need to clean this cache, use a method for it
--- protected/config/console.php
+++ protected/config/console.php
@@ -1,11 +1,13 @@
 <?php
+$db = require '_db.php';
+$db['connectionString'] = preg_filter('|:(/{0,1})protected/|', ':$1', $db['connectionString']);
 
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'WIU CLI',
 
 	'components'=>array(
-		'db'=> require '_db.php',
+		'db'=> $db,
 		'cache' => require '_cache.php',
 	),
 );
