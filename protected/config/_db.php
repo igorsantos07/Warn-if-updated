115a13b695ada957cbda5069861b521a7fddfe9a
igoru <igorsantos07@gmail.com>
2012-07-08 19:54:34 -0300

    * DRYIng the database code

--- protected/config/_db.php
+++ protected/config/_db.php
@@ -2,23 +2,23 @@
 require_once '_vars.php';
 
 if (ENV == 'pagoda') {
-	return array(
+	$db_config = array(
 		'connectionString' => "mysql:host={$_SERVER['DB1_HOST']};mysql:port={$_SERVER['DB1_PORT']};dbname={$_SERVER['DB1_NAME']}",
-		'emulatePrepare' => true,
 		'username' => $_SERVER['DB1_USER'],
 		'password' => $_SERVER['DB1_PASS'],
-		'charset' => 'utf8',
-		'schemaCachingDuration' => 60*60*24*365, //eternal caching. if there's a need to clean this cache, use a method for it
 	);
 }
 else {
-	return array(
+	$db_config = array(
 		'connectionString' => "sqlite:../protected/data/development.db",
-		'emulatePrepare' => true,
-		'charset' => 'utf8',
-		'schemaCachingDuration' => 60*60*24*365, //eternal caching. if there's a need to clean this cache, use a method for it
 	);
 }
-/**
- * @todo DRY this a little bit more
- */
+
+return array_merge(
+	array(
+		'emulatePrepare' => true,
+		'charset' => 'utf8',
+		'schemaCachingDuration' => 60*60*24*365, //eternal caching. if you need to clean this cache, use a method
+	),
+	$db_config
+);
