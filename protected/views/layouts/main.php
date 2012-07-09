745691bfc5a678275d124892c7e4edee67791148
Igoru <igorsantos07@gmail.com>
2012-07-09 01:01:40 -0300

    + adding initial support for language changing in the head of the main layout + adding badger support for the page title * using turbine compiler for CSS calls and calling main.cssp now + adding dumb js/main.js * some more cleanings

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,20 +1,28 @@
+<?php
+	$lang = Yii::app()->language;
+	$app_badger = '['.Yii::app()->name.']';
+	if (strpos(strtolower($this->pageTitle), $app_badger) === false)
+		$this->pageTitle = $this->pageTitle.' '.$app_badger;
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=$lang?>" lang="<?=$lang?>">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+	<meta name="language" content="<?=$lang?>" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+	<title><?=CHtml::encode($this->pageTitle)?></title>
+
 	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<?php
+		Yii::app()->clientScript
+			->registerCssFile('screen.css', 'screen, projection')
+			->registerCssFile('form.css', 'screen, projection')
+			->registerCssFile('print.css', 'print')
+			->registerScriptfile('/js/main.js', CClientScript::POS_END);
+		echo Yii::app()->clientScript->compile('main.cssp');
+	?>
 </head>
 
 <body>
@@ -23,10 +31,10 @@
 
 	<div id="header">
 		<div id="logo">
-			<img src="/img/warn-refresh-072.png" />
+			<img src="/img/warn-refresh-072.png" alt="logo" />
 			<?=Yii::t('app', 'Warn If Updated')?>
 		</div>
-	</div><!-- header -->
+	</div>
 
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
@@ -38,23 +46,30 @@
 //				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+	</div>
 
-	<?php echo $content; ?>
+	<?php
+		if(isset($this->breadcrumbs))
+			$this->widget('zii.widgets.CBreadcrumbs', array('links'=>$this->breadcrumbs));
+
+		if (sizeof($flashes = Yii::app()->user->flashes) > 0) {
+			echo CHtml::openTag('div', array('id' => 'flash-messages'));
+				foreach($flashes as $key => $message)
+					echo CHtml::tag('div', array('class' => "flash-message $key"), $message);
+			echo Chtml::closeTag('div');
+		}
+
+		echo $content;
+	?>
 
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by <a href="http://www.igorsantos.com.br">Igor Santos</a><br/>
+		Copyright &copy; <?=date('Y')?> by <a href="http://www.igorsantos.com.br">Igor Santos</a><br/>
 		All Rights Reserved<br/>
-	</div><!-- footer -->
+	</div>
 
-</div><!-- page -->
+</div>
 
 </body>
 </html>
