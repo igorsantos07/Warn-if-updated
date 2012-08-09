6ec6d9e30f8a2ad600a912598cd4821312314992
igoru <igorsantos07@gmail.com>
2012-08-09 02:31:25 -0300

    + adding last_fetch and last_md5 fields ! fixing relation name

--- protected/models/Page.php
+++ protected/models/Page.php
@@ -6,10 +6,13 @@
  * The followings are the available columns in table 'page':
  * @property integer $id
  * @property string $url
+ * @property integer $last_fetch
+ * @property string last_md5
  */
 class Page extends CActiveRecord {
 
 	public $url = 'http://';
+	public $last_fetch = 0;
 
 	/**
 	 * Returns the static model of the specified AR class.
@@ -31,7 +34,7 @@ class Page extends CActiveRecord {
 
 	public function relations() {
 		return array(
-			'user' => array(self::MANY_MANY, 'User', 'user_page(page_id, user_id)'),
+			'users' => array(self::MANY_MANY, 'User', 'user_page(page_id, user_id)'),
 		);
 	}
 
