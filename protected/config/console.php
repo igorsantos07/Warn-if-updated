9d255825d819e8138bc5792844efc21867ba7084
igoru <igorsantos07@gmail.com>
2012-08-09 03:39:38 -0300

    + adding _vars.php, db, mail, params, import and preload

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,12 +1,34 @@
 <?php
-$db = require '_db.php';
-$db['connectionString'] = preg_replace('|:../protected/|', ':$1', $db['connectionString']);
+require_once '_vars.php';
+
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'WIU CLI',
 
-	'components'=>array(
-		'db'=> $db,
-		'cache' => require '_cache.php',
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading some classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+		'ext.helpers.*',
+		'ext.TurbineCSS.*',
+		'ext.pear.*',
 	),
+
+	'components'=>array(
+		'db'=> require '_db.php',
+		'cache' => require '_cache.php',
+
+		'mail' => array(
+			'class' => 'ext.mail.YiiMail',
+			'logging' => true,
+			'dryRun' => PRODUCTION,
+			'transportType' => 'smtp',
+			'transportOptions' => require '_email.php',
+		),
+	),
+
+	'params' => require '_params.php',
 );
