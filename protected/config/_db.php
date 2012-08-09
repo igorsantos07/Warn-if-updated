b167877b9348a085802e0431dae0561701e909fa
igoru <igorsantos07@gmail.com>
2012-08-09 02:30:40 -0300

    * changing migration as we won't be using sqlite anymore

--- protected/config/_db.php
+++ protected/config/_db.php
@@ -2,23 +2,23 @@
 require_once '_vars.php';
 
 if (ENV == 'pagoda') {
-	$db_config = array(
-		'connectionString' => "mysql:host={$_SERVER['DB1_HOST']};mysql:port={$_SERVER['DB1_PORT']};dbname={$_SERVER['DB1_NAME']}",
-		'username' => $_SERVER['DB1_USER'],
-		'password' => $_SERVER['DB1_PASS'],
-	);
+		$host = $_SERVER['DB1_HOST'];
+		$port = $_SERVER['DB1_PORT'];
+		$name = $_SERVER['DB1_NAME'];
+		$user = $_SERVER['DB1_USER'];
+		$pass = $_SERVER['DB1_PASS'];
 }
 else {
-	$db_config = array(
-		'connectionString' => "sqlite:../protected/data/development.db",
-	);
+	$host = 'localhost';
+	$port = 3306;
+	$name = $user = $pass = 'wiu';
 }
 
-return array_merge(
-	array(
-		'emulatePrepare' => true,
-		'charset' => 'utf8',
-		'schemaCachingDuration' => 60*60*24*365, //eternal caching. if you need to clean this cache, use a method
-	),
-	$db_config
+return array(
+	'connectionString' => "mysql:host=$host;mysql:port=$port;dbname=$name",
+	'username' => $user,
+	'password' => $pass,
+	'emulatePrepare' => true,
+	'charset' => 'utf8',
+	'schemaCachingDuration' => 60*60*24*365, //eternal caching. if you need to clean this cache, use a method
 );
--- protected/migrations/m120706_220338_add_page_user.php
+++ protected/migrations/m120706_220338_add_page_user.php
@@ -3,24 +3,21 @@
 class m120706_220338_add_page_user extends CDbMigration {
 
 	public function up() {
-		$engine = PRODUCTION? 'ENGINE = InnoDB' : '';
-		$pk_type = PRODUCTION? 'INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT' : 'INTEGER PRIMARY KEY AUTOINCREMENT';
-
 		$this->createTable('page', array(
-			'id' => $pk_type,
+			'id' => 'INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
 			'url' => 'VARCHAR(255) NOT NULL',
             'last_fetch' => 'TIMESTAMP NOT NULL DEFAULT 0',
             'last_md5' => 'CHAR(32)',
-		), $engine);
+		), 'ENGINE = InnoDB');
         $this->createIndex('uq_page_url', 'page', 'url', true);
 
 		$this->createTable('user', array(
-			'id' => $pk_type,
+			'id' => 'INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
 			'name' => 'VARCHAR(50) NOT NULL',
             'email' => 'VARCHAR(50) NOT NULL',
             'username' => 'VARCHAR(25) NOT NULL',
             'password' => 'CHAR(40) NOT NULL',
-		), $engine);
+		), 'ENGINE = InnoDB');
         $this->createIndex('uq_user_email', 'user', 'email', true);
         $this->createIndex('uq_user_username', 'user', 'username', true);
 
@@ -28,12 +25,10 @@ class m120706_220338_add_page_user extends CDbMigration {
 			'user_id' => 'INT UNSIGNED NOT NULL',
 			'page_id' => 'INT UNSIGNED NOT NULL',
             'PRIMARY KEY (user_id, page_id)',
-		), $engine);
+		), 'ENGINE = InnoDB');
         $this->createIndex('uq_user_page', 'user_page', 'user_id, page_id', true);
-		if (PRODUCTION) {
-			$this->addForeignKey('fk_user_page_page', 'user_page', 'page_id', 'page', 'id');
-			$this->addForeignKey('fk_user_page_user', 'user_page', 'user_id', 'user', 'id');
-		}
+		$this->addForeignKey('fk_user_page_page', 'user_page', 'page_id', 'page', 'id');
+		$this->addForeignKey('fk_user_page_user', 'user_page', 'user_id', 'user', 'id');
 	}
 
 
