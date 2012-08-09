cae6fd113c5ea305aed2b12fb92bffa6b395eb71
igoru <igorsantos07@gmail.com>
2012-08-09 03:49:05 -0300

    * fixing email/password

--- protected/config/_email.php
+++ protected/config/_email.php
@@ -1,7 +1,8 @@
 <?php return array(
 	'host' => 'smtp.gmail.com',
 	'encryption' => 'tls',
-	'username' => 'igorsantos07@gmail.com',
-	'password' => 'go24121960',
+	'username' => 'warnifupdated@gmail.com',
+	/** @todo TAKE THIS PASSWORD FROM HERE! D= */
+	'password' => 'warn2012',
 	'port' => 465,
 );
